90ab1594bd50cd3d41785b7252a1a91d6905bb4b
Dmitriy Bondarenko <dabwork1982@gmail.com>
2022-05-18 01:26:05 +0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import { Box, Container, Typography, Link } from '@mui/material';
 
-const Footer = () => {
-  return (
-    <footer>
-      <Box
-        sx={{
-          py: { xs: 2, sm: 3 },
-          color: '#010163',
-        }}
-      >
-        <Container maxWidth="lg">
-          <Typography textAlign="center" variant="body1">
-            CodiWea&deg; &copy; {new Date().getFullYear()} by{' '}
-            <Link
-              href="https://github.com/dab82"
-              underline="hover"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              dab82
-            </Link>
-          </Typography>
-        </Container>
-      </Box>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer>
+    <Box
+      sx={{
+        py: { xs: 2, sm: 3 },
+        color: '#010163',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography textAlign="center" variant="body1">
+          CodiWea&deg; &copy; {new Date().getFullYear()} by{' '}
+          <Link
+            href="https://github.com/dab82"
+            underline="hover"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            dab82
+          </Link>
+        </Typography>
+      </Container>
+    </Box>
+  </footer>
+);
 
 export default Footer;
