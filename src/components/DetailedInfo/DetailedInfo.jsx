df3934fb0ebb29208af5cac4d187995e062f22d1
Dmitriy Bondarenko <dabwork1982@gmail.com>
2022-05-18 01:25:46 +0300

    fix

--- src/components/DetailedInfo/DetailedInfo.jsx
+++ src/components/DetailedInfo/DetailedInfo.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { getHourlyWeather, updateCity } from 'redux/operations';
 import {
   Box,
+  Grid,
   Container,
   IconButton,
   Typography,
@@ -20,7 +21,7 @@ const DetailedInfo = () => {
   const idCity = useSelector(state => state.cities.cityId);
   const hourlyTempList = useSelector(state => state.cities.hourlyWeatherCity);
 
-  const hourlyTemp24 = hourlyTempList.slice(0, 25);
+  const hourlyTemp12 = hourlyTempList.slice(1, 13);
 
   const loading = useSelector(state => state.cities.loading);
 
@@ -33,14 +34,36 @@ const DetailedInfo = () => {
   }, [coord, dispatch]);
 
   const iconsWeather = weather.map(({ icon }) => icon);
-  const currentTemp = Math.floor(temp - 273);
-  const positiveTemp = currentTemp > 0;
-  const timeNow = new Date(dt * 1000).toLocaleTimeString();
+  const currentTemp = (temp - 273).toFixed(1);
+  const currentTime = new Date(dt * 1000).toLocaleTimeString();
   const sunR = new Date(sys.sunrise * 1000).toLocaleTimeString();
   const sunS = new Date(sys.sunset * 1000).toLocaleTimeString();
 
   const updateData = () => {
     dispatch(updateCity(name));
+  };
+  // форматирование даты
+  const format_date = d => {
+    let months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    return `${month} ${date} , ${day}. `;
   };
 
   return (
@@ -56,11 +79,27 @@ const DetailedInfo = () => {
       </IconButton>
       <Box>
         <Box>
-          <Typography variant="h4" noWrap>
+          {' '}
+          <div className="date">{format_date(new Date())}</div>
+          <Typography variant="h3" noWrap>
             {name}, {sys.country}{' '}
           </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography variant="h3" noWrap>
+              {currentTemp > 0 ? '+' : '-'}
+              {currentTemp} &deg;C
+            </Typography>
+            <Typography variant="h6">
+              <div>
+                <img
+                  src={`http://openweathermap.org/img/wn/${iconsWeather[0]}@2x.png`}
+                  alt={iconsWeather[0]}
+                />
+              </div>
+            </Typography>
+          </Box>
           <Typography variant="h6" noWrap>
-            last update {timeNow}
+            last update {currentTime}
           </Typography>
           <Typography variant="h6" noWrap>
             sunrise: {sunR}
@@ -70,12 +109,8 @@ const DetailedInfo = () => {
           </Typography>
         </Box>
         <Box>
-          <Typography variant="h4" noWrap>
-            {positiveTemp ? '+' : '-'}
-            {currentTemp} C°
-          </Typography>
           <Typography variant="h4">
-            {`${Math.floor((pressure * 7.464) / 10)} mm.Hg`}
+            {`${Math.round((pressure * 7.464) / 10)} mm.Hg`}
           </Typography>
           <Typography variant="h4" noWrap>
             {humidity} %
@@ -84,14 +119,6 @@ const DetailedInfo = () => {
         <Box>
           <Typography variant="h6" noWrap>
             Clouds {clouds.all} %
-          </Typography>
-          <Typography variant="h6">
-            <div>
-              <img
-                src={`http://openweathermap.org/img/wn/${iconsWeather[0]}@2x.png`}
-                alt={iconsWeather[0]}
-              />
-            </div>
           </Typography>
           <Typography variant="h6" noWrap>
             wind speed: {wind.speed} m/s
@@ -103,44 +130,46 @@ const DetailedInfo = () => {
         <CircularProgress color="secondary" />
       ) : (
         <Box>
-          <List
+          <Typography variant="h5">Hourly forecast:</Typography>
+          <Grid
+            container
+            spacing={1}
             sx={{
               display: 'flex',
               alignItems: 'center',
-              justifyContent: 'space-around',
+              justifyContent: 'center',
             }}
           >
-            {hourlyTemp24.map((hour, idx) => {
+            {hourlyTemp12.map((hour, idx) => {
               let timeHour = new Date(hour.dt * 1000).getHours();
 
               if (timeHour < 10) {
                 timeHour = `0${timeHour}`;
               }
 
-              const red = 125;
-              const green = 125;
-              const blue = 255;
-
-              const tempColor = hour.temp - 273;
-              const x = (tempColor / 10) * 100;
-              const r = Math.floor(((255 - 125) * x) / 100);
-
               return (
-                <li key={hour.dt}>
-                  <p
-                    style={{
-                      marginTop: `${40 - Math.floor(hour.temp - 273)}px`,
-                      backgroundColor: `rgb(${red + r}, ${green}, ${blue - r})`,
+                <Grid item xs={1} key={hour.dt}>
+                  <Typography
+                    sx={{
+                      py: 1,
+                      textAlign: 'center',
+                      marginTop: `${50 - Math.round(hour.temp - 273)}px`,
+                      backgroundColor: '#f1f38c',
+                      borderRadius: '1px',
+                      boxShadow: '1px 1px rgba(160, 105, 4, 0.3)',
                     }}
                   >
-                    {Math.floor(hour.temp - 273)}
-                  </p>
+                    {Math.round(hour.temp - 273) > 0 ? '+' : '-'}
+                    {Math.round(hour.temp - 273)}
+                  </Typography>
 
-                  <p>{timeHour}:00</p>
-                </li>
+                  <Typography sx={{ fontSize: 'small', textAlign: 'center' }}>
+                    {timeHour}:00
+                  </Typography>
+                </Grid>
               );
             })}
-          </List>
+          </Grid>
         </Box>
       )}
     </Container>
